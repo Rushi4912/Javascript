025bc28d7f8a58c5893c532697b7c8160f5c083b
Rushi4912 <pawarrushikeshpawar@gmail.com>
2023-12-24 19:43:54 +0530

    change

--- Array/splice.js
+++ Array/splice.js
@@ -21,6 +21,3 @@ let myArray3 = ["food", "newOne"];
 // from index 2
 // delete 0
 // then insert "complex" and "language"
-arr.splice(2, 0, "complex", "language");
-
-console.log(arr); // "I", "study", "complex", "language", "JavaScript"
