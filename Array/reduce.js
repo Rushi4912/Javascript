7db80a9832a4b4ffbddc4bb4aa8788fe2cdbd5f9
Rushi4912 <pawarrushikeshpawar@gmail.com>
2023-12-26 22:49:33 +0530

    change

--- Array/reduce.js
+++ Array/reduce.js
@@ -2,9 +2,3 @@
 // the first argument is essentially the accumulator that stores the combined result of all previous executions. And at the end it becomes the result of reduce.
 
 let arr = [1, 2, 3, 4, 5];
-
-let ans = arr.reduce(function (acc, current) {
-     return acc + current;
-});
-console.log(ans);
-// in this method  result of the previous call becomes the first argument of the next one.
