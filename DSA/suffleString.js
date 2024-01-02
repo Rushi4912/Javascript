4b93535dc2f7529abf6de125a25607082b18a527
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-02 21:50:22 +0530

    change

--- DSA/suffleString.js
+++ DSA/suffleString.js
@@ -5,7 +5,13 @@ var restoreString = function(s, indices) {
 
   for (let i = 0; i < n; i++) {
     shuffled[indices[i]] = s[i];
+    console.log("hi");
   }
 
   return shuffled.join('');
 };
+const array = ["apple", "new"];
+for (let i = 0; i <= 10; i++){
+
+  console.log("hi there");
+}
