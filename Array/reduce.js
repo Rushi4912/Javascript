e6f04e33659c37603f6c96d5bbbc004a01bc14ed
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-06 18:28:31 +0530

    change

--- Array/reduce.js
+++ Array/reduce.js
@@ -2,3 +2,7 @@
 // the first argument is essentially the accumulator that stores the combined result of all previous executions. And at the end it becomes the result of reduce.
 
 let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i <= 10; i++){
+
+     console.log("hi there");
+}
